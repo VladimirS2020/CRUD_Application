b729faa6b0e552c4aed5c848546550faa9d0c114
Vladimir Shcherban <vladimir.shcherban@yandex.ru>
2021-06-14 18:49:20 +0300

    Commit 2

--- src/main/java/ru/shcherban/springapplication/controllers/PeopleController.java
+++ src/main/java/ru/shcherban/springapplication/controllers/PeopleController.java
@@ -2,9 +2,12 @@ package ru.shcherban.springapplication.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.shcherban.springapplication.dao.PersonDAO;
 import ru.shcherban.springapplication.models.Person;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
@@ -29,30 +32,40 @@ public class PeopleController {
     }
 
     @GetMapping("/new")
-    public String newPerson(@ModelAttribute("personToBeDeliveredInThymeleaf") Person person) {
+    public String newPerson(@ModelAttribute("person") Person person) {
         return "people/new";
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("person_from_Browser") Person personCreated) {
+    public String create(@ModelAttribute("person") @Valid Person personCreated,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "people/new";
+        }
+
         personDAO.save(personCreated);
         return "redirect:/people";
     }
 
     @GetMapping("/{id}/edit")
     public String edit(Model model, @PathVariable("id") int id) {
-        model.addAttribute("personToBeDeliveredInHTMLEditFile", personDAO.show(id));
+        model.addAttribute("person", personDAO.show(id));
         return "people/edit";
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("personToBeDeliveredInHTMLEditFile") Person person, @PathVariable("id") int id) {
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if (bindingResult.hasErrors()) {
+            return "people/edit";
+        }
+
         personDAO.update(id, person);
         return "redirect:/people";
     }
 
     @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") int id){
+    public String delete(@PathVariable("id") int id) {
         personDAO.delete(id);
         return "redirect:/people";
     }
